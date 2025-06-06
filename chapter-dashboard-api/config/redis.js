2eefa2b621a77ce99837c02192ad7910fb9e68a1
reckt32 <divyanshsingh5nov2003@gmail.com>
2025-06-06 13:52:58 +0530

    redis

--- chapter-dashboard-api/config/redis.js
+++ chapter-dashboard-api/config/redis.js
@@ -1,6 +1,6 @@
 const redis = require('redis');
 
-const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+const REDIS_URL = process.env.REDIS_URL;
 
 const redisClient = redis.createClient({ url: REDIS_URL });
 
